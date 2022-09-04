9adb32a42e1892f0f7a81b21ad1ff41fc3f629aa
cloudsw <61484036+cloudsw@users.noreply.github.com>
2022-09-04 12:48:15 +0800

    backend

--- kg/src/main/java/com/kg/backend/service/impl/user/account/InfoServiceImpl.java
+++ kg/src/main/java/com/kg/backend/service/impl/user/account/InfoServiceImpl.java
@@ -1,14 +1,28 @@
 package com.kg.backend.service.impl.user.account;
 
+import com.kg.backend.pojo.User;
+import com.kg.backend.service.impl.utils.UserDetailsImpl;
 import com.kg.backend.service.user.account.InfoService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class InfoServiceImpl implements InfoService {
     @Override
     public Map<String, String> getInfo() {
-        return null;
+        UsernamePasswordAuthenticationToken authentication = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl loginUser = (UserDetailsImpl) authentication.getPrincipal();
+        User user = loginUser.getUser();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error_message", "success");
+        map.put("id", String.valueOf(user.getId()));
+        map.put("username", user.getUsername());
+        map.put("photo", user.getPhoto());
+        return map;
     }
 }
